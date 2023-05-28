bf1a20fb49c098f986d9b28fbc39f6521f925bbc
retini <andrea.retini@gmail.com>
2023-05-28 18:46:06 +0200

    Improve readability of `escapeNonPrintableChars`

--- src/utils/chars.ts
+++ src/utils/chars.ts
@@ -1,17 +1,30 @@
+enum AsciiCharCodes {
+	SPACE = 32,
+	TAB = 9,
+	NEW_LINE = 10,
+	CARRIAGE_RETURN = 13,
+	DELETE = 127,
+}
+
+// Return the escaped version of the non-printable characters contained in the
+// provided string.
 function escapeNonPrintableChars(char: string): string {
-	let charCode = char.charCodeAt(0);
-	if (charCode === 32) {
-		return '\\s';
-	} else if (charCode === 9) {
-		return '\\t';
-	} else if (charCode === 10) {
-		return '\\n';
-	} else if (charCode === 13) {
-		return '\\r';
-	} else if (charCode < 32 || charCode === 127) {
-		return '\\x' + charCode.toString(16).padStart(2, '0');
+	const charCode = char.charCodeAt(0);
+	switch (charCode) {
+		case AsciiCharCodes.SPACE:
+			return '\\s';
+		case AsciiCharCodes.TAB:
+			return '\\t';
+		case AsciiCharCodes.NEW_LINE:
+			return '\\n';
+		case AsciiCharCodes.CARRIAGE_RETURN:
+			return '\\r';
+		default:
+			if (charCode < AsciiCharCodes.SPACE || charCode === AsciiCharCodes.DELETE) {
+				return '\\x' + charCode.toString(16).padStart(2, '0');
+			}
+			return char;
 	}
-	return char;
 }
 
 export { escapeNonPrintableChars };
